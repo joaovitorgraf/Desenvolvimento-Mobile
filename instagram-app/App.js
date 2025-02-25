021a4124c6c9e62b5473dbbe98f88456dc62943d
joaovitorgraf <grafjoaovitor@gmail.com>
2025-02-24 22:04:54 -0300

    add fotos do feed

--- instagram-app/App.js
+++ instagram-app/App.js
@@ -1,50 +1,275 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView, useWindowDimensions } from 'react-native';
 import Feather from '@expo/vector-icons/Feather';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import Entypo from '@expo/vector-icons/Entypo';
 
 export default function App() {
+    const { width } = useWindowDimensions();
+
     return (
         <SafeAreaView style={styles.container}>
-            <View
-                style={{
-                    padding: 15,
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                }}
-            >
-                <Text style={styles.text}>Instagram</Text>
+            <ScrollView>
                 <View
                     style={{
+                        padding: 15,
                         flexDirection: 'row',
                         alignItems: 'center',
-                        gap: 15,
+                        justifyContent: 'space-between',
                     }}
                 >
-                    <AntDesign name="plussquareo" size={24} color="black" />
-                    <Feather name="heart" size={24} color="black" />
-                    <FontAwesome6 name="facebook-messenger" size={24} color="black" />
+                    <Text style={{ fontSize: 30 }}>Instagram</Text>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            gap: 15,
+                        }}
+                    >
+                        <AntDesign name="plussquareo" size={30} color="black" />
+                        <Feather name="heart" size={30} color="black" />
+                        <FontAwesome6 name="facebook-messenger" size={30} color="black" />
+                    </View>
                 </View>
-                <View style={{ flexDirection: 'row' }}>
-                    <Image source={require('./assets/favicon.png')} />
-                </View>
-                <View style={{ flexDirection: 'row' }}></View>
-                <StatusBar style="auto" />
-            </View>
+
+                <ScrollView horizontal={true} indicatorStyle={'white'}>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            padding: 15,
+                            gap: 15,
+                            borderBottomWidth: 1,
+                            borderColor: '#ccc',
+                        }}
+                    >
+                        <View>
+                            <Image
+                                style={{
+                                    backgroundColor: '#ccc',
+                                    width: 70,
+                                    height: 70,
+                                    borderRadius: '50%',
+                                }}
+                                source={require('./assets/favicon.png')}
+                            />
+                            <Text
+                                numberOfLines={1}
+                                ellipsizeMode="tail"
+                                style={{
+                                    width: 70,
+                                    textAlign: 'center',
+                                }}
+                            >
+                                Lorem Ipsum
+                            </Text>
+                        </View>
+                        <View>
+                            <Image
+                                style={{
+                                    backgroundColor: '#ccc',
+                                    width: 70,
+                                    height: 70,
+                                    borderRadius: '50%',
+                                }}
+                                source={require('./assets/favicon.png')}
+                            />
+                            <Text
+                                numberOfLines={1}
+                                ellipsizeMode="tail"
+                                style={{
+                                    width: 70,
+                                    textAlign: 'center',
+                                }}
+                            >
+                                Lorem Ipsum
+                            </Text>
+                        </View>
+                        <View>
+                            <Image
+                                style={{
+                                    backgroundColor: '#ccc',
+                                    width: 70,
+                                    height: 70,
+                                    borderRadius: '50%',
+                                }}
+                                source={require('./assets/favicon.png')}
+                            />
+                            <Text
+                                numberOfLines={1}
+                                ellipsizeMode="tail"
+                                style={{
+                                    width: 70,
+                                    textAlign: 'center',
+                                }}
+                            >
+                                Lorem Ipsum
+                            </Text>
+                        </View>
+                        <View>
+                            <Image
+                                style={{
+                                    backgroundColor: '#ccc',
+                                    width: 70,
+                                    height: 70,
+                                    borderRadius: '50%',
+                                }}
+                                source={require('./assets/favicon.png')}
+                            />
+                            <Text
+                                numberOfLines={1}
+                                ellipsizeMode="tail"
+                                style={{
+                                    width: 70,
+                                    textAlign: 'center',
+                                }}
+                            >
+                                Lorem Ipsum
+                            </Text>
+                        </View>
+                        <View>
+                            <Image
+                                style={{
+                                    backgroundColor: '#ccc',
+                                    width: 70,
+                                    height: 70,
+                                    borderRadius: '50%',
+                                }}
+                                source={require('./assets/favicon.png')}
+                            />
+                            <Text
+                                numberOfLines={1}
+                                ellipsizeMode="tail"
+                                style={{
+                                    width: 70,
+                                    textAlign: 'center',
+                                }}
+                            >
+                                Lorem Ipsum
+                            </Text>
+                        </View>
+                        <View>
+                            <Image
+                                style={{
+                                    backgroundColor: '#ccc',
+                                    width: 70,
+                                    height: 70,
+                                    borderRadius: '50%',
+                                }}
+                                source={require('./assets/favicon.png')}
+                            />
+                            <Text
+                                numberOfLines={1}
+                                ellipsizeMode="tail"
+                                style={{
+                                    width: 70,
+                                    textAlign: 'center',
+                                }}
+                            >
+                                Lorem Ipsum
+                            </Text>
+                        </View>
+                    </View>
+                </ScrollView>
+
+                <>
+                    <View
+                        style={{
+                            padding: 15,
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <View
+                            style={{
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Image
+                                style={{
+                                    backgroundColor: '#ccc',
+                                    width: 45,
+                                    height: 45,
+                                    borderRadius: '50%',
+                                }}
+                                source={require('./assets/favicon.png')}
+                            />
+                            <Text
+                                style={{
+                                    fontSize: 20,
+                                    fontWeight: 'bold',
+                                    marginLeft: 10,
+                                }}
+                            >
+                                Fulano da Silva
+                            </Text>
+                        </View>
+                        <Entypo name="dots-three-horizontal" size={24} color="black" />
+                    </View>
+                    <Image
+                        style={{
+                            backgroundColor: '#ccc',
+                            width: width,
+                            height: width,
+                        }}
+                        source={require('./assets/favicon.png')}
+                    />
+                </>
+                <>
+                    <View
+                        style={{
+                            padding: 15,
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <View
+                            style={{
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Image
+                                style={{
+                                    backgroundColor: '#ccc',
+                                    width: 45,
+                                    height: 45,
+                                    borderRadius: '50%',
+                                }}
+                                source={require('./assets/favicon.png')}
+                            />
+                            <Text
+                                style={{
+                                    fontSize: 20,
+                                    fontWeight: 'bold',
+                                    marginLeft: 10,
+                                }}
+                            >
+                                Fulano da Silva
+                            </Text>
+                        </View>
+                        <Entypo name="dots-three-horizontal" size={24} color="black" />
+                    </View>
+                    <Image
+                        style={{
+                            backgroundColor: '#ccc',
+                            width: width,
+                            height: width,
+                        }}
+                        source={require('./assets/favicon.png')}
+                    />
+                </>
+            </ScrollView>
+            <StatusBar style="auto" />
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        // alignItems: 'center',
-        // justifyContent: 'center',
-    },
-    text: {
-        fontSize: 30,
+        backgroundColor: 'white',
     },
 });
