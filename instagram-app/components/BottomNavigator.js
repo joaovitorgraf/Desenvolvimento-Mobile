44c84c019fa376f467be38922d45a4df106b5fda
João Graf <grafjoaovitor@gmail.com>
2025-03-10 21:55:22 -0300

    colocando icons

--- instagram-app/components/BottomNavigator.js
+++ instagram-app/components/BottomNavigator.js
@@ -4,17 +4,18 @@ import SearchScreen from "../screens/SearchScreen";
 import CreateScreen from "../screens/CreateScreen";
 import ReelsScreen from "../screens/ReelsScreen";
 import AccountScreen from "../screens/AccountScreen";
+import { Entypo, Feather } from "@expo/vector-icons";
 
 export default function BottomNavigator() {
   const Tab = createBottomTabNavigator();
 
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-      <Tab.Screen name="Search" component={SearchScreen} options={{ headerShown: false }} />
-      <Tab.Screen name="Create" component={CreateScreen} options={{ headerShown: false }} />
-      <Tab.Screen name="Reels" component={ReelsScreen} options={{ headerShown: false }} />
-      <Tab.Screen name="Account" component={AccountScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false, title: "", tabBarIcon: ({ color, size }) => <Entypo name="home" color={color} size={30} /> }} />
+      <Tab.Screen name="Search" component={SearchScreen} options={{ headerShown: false, title: "", tabBarIcon: ({ color, size }) => <Feather name="search" color={color} size={30} /> }} />
+      <Tab.Screen name="Create" component={CreateScreen} options={{ headerShown: false, title: "", tabBarIcon: ({ color, size }) => <Entypo name="home" color={color} size={30} /> }} />
+      <Tab.Screen name="Reels" component={ReelsScreen} options={{ headerShown: false, title: "", tabBarIcon: ({ color, size }) => <Entypo name="home" color={color} size={30} /> }} />
+      <Tab.Screen name="Account" component={AccountScreen} options={{ headerShown: false, title: "", tabBarIcon: ({ color, size }) => <Entypo name="home" color={color} size={30} /> }} />
     </Tab.Navigator>
   );
 }
