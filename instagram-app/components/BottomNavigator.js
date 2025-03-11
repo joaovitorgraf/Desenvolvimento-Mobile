bcb0c1329a380df11576c8f029a9f772ff2b7cf8
João Graf <grafjoaovitor@gmail.com>
2025-03-10 21:45:12 -0300

    tirando header do tab

--- instagram-app/components/BottomNavigator.js
+++ instagram-app/components/BottomNavigator.js
@@ -1,12 +1,20 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
+import SearchScreen from "../screens/SearchScreen";
+import CreateScreen from "../screens/CreateScreen";
+import ReelsScreen from "../screens/ReelsScreen";
+import AccountScreen from "../screens/AccountScreen";
 
 export default function BottomNavigator() {
   const Tab = createBottomTabNavigator();
 
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="Search" component={SearchScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="Create" component={CreateScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="Reels" component={ReelsScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="Account" component={AccountScreen} options={{ headerShown: false }} />
     </Tab.Navigator>
   );
 }
---
+++ instagram-app/screens/CreateScreen.js
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function CreateScreen() {
+  return <Text>Tela de Create</Text>;
+}
