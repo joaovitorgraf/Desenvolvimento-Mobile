1739f1809db3b07a4774d4aacfa40984e45231fe
João Graf <grafjoaovitor@gmail.com>
2025-03-10 22:09:04 -0300

    criando caixa de pesquisa

--- instagram-app/screens/SearchScreen.js
+++ instagram-app/screens/SearchScreen.js
@@ -1,5 +1,18 @@
-import { Text } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, TextInput } from "react-native";
 
 export default function SearchScreen() {
-  return <Text>Tela de Pesquisa</Text>;
+  const { searchText, setSearchText } = useState("");
+
+  useEffect(() => {
+    console.log(searchText);
+  }, [searchText]);
+
+  return (
+    <>
+      <Text>Tela de pesquisa</Text>
+      <TextInput style={{ height: 40, margin: 12, borderWidth: 1, padding: 10 }} value={searchText} onChangeText={setSearchText} />
+      <Text>{searchText}</Text>
+    </>
+  );
 }
